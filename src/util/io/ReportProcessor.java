8734eff97d780f3175c082b50f06579bffa23b07
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-08-22 02:14:04 -0300

    Processor now only keeps target metrics. Intermediary file now created

--- src/util/io/ReportProcessor.java
+++ src/util/io/ReportProcessor.java
@@ -8,18 +8,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class ReportProcessor {
 	private BufferedReader reader;
-	private String model;
-    private Map<String, BufferedWriter> loggers;
-    
+	private BufferedWriter logger;
+
 	public ReportProcessor() {
-		this.model = model;
 	}
 
 	private void createReader(String filePath) {
@@ -34,84 +28,49 @@ public class ReportProcessor {
 
 	}
 
-	private BufferedWriter createLogger(String filePath) {
-		BufferedWriter logger = null;
+	private void createLogger(String filePath) {
 		try {
 			FileOutputStream fs = new FileOutputStream(filePath);
 			OutputStreamWriter sr = new OutputStreamWriter(fs);
-			logger = new BufferedWriter(sr);
+			this.logger = new BufferedWriter(sr);
 
 		} catch (FileNotFoundException e) {
 
 		}
-		return logger;
 
 	}
 
-	public void createLoggers(String extension) {
-		this.loggers = new TreeMap<String, BufferedWriter>();
+	public void filterTargetMetrics(String inputFilePath, String outputFilePath) {
 		try {
-			this.loggers.put("\"Number Waiting\" \"ResourceApp\"", createLogger("NumberWaitingAlterar" + model + extension));
-			this.loggers.put("\"Number Waiting\" \"ResourceLogin\"", createLogger("NumberWaitingAlterar" + model + extension));
-			this.loggers.put("\"Number Waiting\" \"ResourceConsultar\"", createLogger("NumberWaitingAlterar" + model +  extension));
-			this.loggers.put("\"Number Waiting\" \"ResourceAlterar\"", createLogger("NumberWaitingAlterar" + model +  extension));
-			this.loggers.put("\"Number Waiting\" \"ResourceBD\"", createLogger("NumberWaitingAlterar" + model +  extension));
-			this.loggers.put("\"Instantaneous Utilization\" \"ResourceApp\"", createLogger("UtilizationApp" + model + extension));
-			this.loggers.put("\"Instantaneous Utilization\" \"ResourceLogin\"", createLogger("UtilizationLogin" + model + extension));
-			this.loggers.put("\"Instantaneous Utilization\" \"ResourceConsultar\"", createLogger("UtilizationConsultar" + model +  extension));
-			this.loggers.put("\"Instantaneous Utilization\" \"ResourceAlterar\"", createLogger("UtilizationAlterar" + model +  extension));
-			this.loggers.put("\"Instantaneous Utilization\" \"ResourceBD\"", createLogger("UtilizationBD" + model +  extension));
-			this.loggers.put("\"Waiting Time\" \"ResourceApp\"", createLogger("NumberWaitingApp" + model + extension));
-			this.loggers.put("\"Waiting Time\" \"ResourceLogin\"", createLogger("WaitingTimeLogin" + model + extension));
-			this.loggers.put("\"Waiting Time\" \"ResourceConsultar\"", createLogger("WaitingTimeConsultar" + model +  extension));
-			this.loggers.put("\"Waiting Time\" \"ResourceAlterar\"", createLogger("WaitingTimeAlterar" + model +  extension));
-			this.loggers.put("\"Waiting Time\" \"ResourceBD\"", createLogger("WaitingTimeBD" + model +  extension));
-			
-			
-		} catch (Exception e) {
-		}
-	}
+			this.createReader(inputFilePath);
+			this.createLogger(outputFilePath);
 
-	public String getModel() {
-		return this.model;
-	}
-
-	public void setModel(String newModel) {
-		this.model = newModel;
-	}
-
-	public void processReport(String filePath) {
-		try {
-			this.createReader(filePath);
-			
 			String line;
 			while ((line = this.reader.readLine()) != null) {
 				String[] fragmented = line.split(",");
-
+				if (fragmented[20].equals("\"Waiting Time\"") || fragmented[20].equals("\"Number Waiting\"")
+						|| fragmented[20].equals("\"Instantaneous Utilization\"")) {
+					this.logger.write(fragmented[20].substring(1, fragmented[20].length() - 1) + ","
+							+ fragmented[26].substring(1, fragmented[26].length() - 1) + "," + fragmented[28]);
+					this.logger.newLine();
+				}
 			}
 
 		} catch (Exception e) {
+		} finally {
+			try {
+				this.logger.close();
+				this.reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
 
 	public static void main(String[] args) throws IOException {
-		FileInputStream fs = null;
-		try {
-			fs = new FileInputStream("REPLICATION.csv");
-			InputStreamReader sr = new InputStreamReader(fs);
-			BufferedReader bf = new BufferedReader(sr);
-
-			for (int i = 0; i < 100; i++) {
-				String str = bf.readLine();
-				String[] strList = str.split(",");
-				System.out.println(strList[20] + " " + strList[26] + " " + strList[28]);
-			}
-
-		} catch (Exception e) {
-		} finally {
-			fs.close();
-		}
+		ReportProcessor rp = new ReportProcessor();
+		rp.filterTargetMetrics("COMPLETE_RESULTS_LOW.csv", "INTERMEDIARY_RESULTS_LOW.csv");
 	}
 
 }

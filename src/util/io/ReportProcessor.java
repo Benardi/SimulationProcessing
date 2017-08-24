8881a20c47e58e66eb95969efe4fa6e623d036db
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-08-24 20:57:19 -0300

    Temporary files are now being deleted after processed

--- src/util/io/ReportProcessor.java
+++ src/util/io/ReportProcessor.java
@@ -9,20 +9,26 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class ReportProcessor {
 	private BufferedReader reader;
 	public final static String TEMP_FILE_PREFIX = "TMPY";
+	private Deque<String> deleteBin;
 
 	public ReportProcessor() {
+		this.deleteBin = new ArrayDeque<String>();
 	}
 
 	public void processReport(String inputFilePath, String batchName, String deviceNames) {
 		filterTargetMetrics(inputFilePath, batchName);
 		segregateDevices(batchName, deviceNames);
 		segregateMetrics(batchName, deviceNames);
+		emptyDeleteBin();
 
 	}
 
@@ -65,6 +71,7 @@ public class ReportProcessor {
 		try {
 			this.createReader(inputFilePath);
 			logger = createLogger("src/results/" + demand + "/" + TEMP_FILE_PREFIX + "RESULTS.csv");
+			this.deleteBin.add("src/results/" + demand + "/" + TEMP_FILE_PREFIX + "RESULTS.csv");
 
 			String line;
 			while ((line = this.reader.readLine()) != null) {
@@ -98,6 +105,18 @@ public class ReportProcessor {
 		}
 
 	}
+	
+	private void emptyDeleteBin(){
+		while(!this.deleteBin.isEmpty()){
+			try {
+				Path path = Paths.get(this.deleteBin.pop());
+				Files.delete(path);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		
+	}
 
 	public void segregateDevices(String demand, String deviceNames) {
 		String[] listOfDevices = deviceNames.split(",");
@@ -113,14 +132,28 @@ public class ReportProcessor {
 			this.createReader("src/results/" + demand + "/" + TEMP_FILE_PREFIX + "RESULTS.csv");
 			loggerApp = createLogger("src/results/" + demand + "/" + listOfDevices[0] + "/" + TEMP_FILE_PREFIX
 					+ listOfDevices[0] + ".csv");
+			this.deleteBin.add("src/results/" + demand + "/" + listOfDevices[0] + "/" + TEMP_FILE_PREFIX
+					+ listOfDevices[0] + ".csv");
 			loggerLogin = createLogger("src/results/" + demand + "/" + listOfDevices[1] + "/" + TEMP_FILE_PREFIX
 					+ listOfDevices[1] + ".csv");
+			this.deleteBin.add("src/results/" + demand + "/" + listOfDevices[1] + "/" + TEMP_FILE_PREFIX
+					+ listOfDevices[1] + ".csv");
+
 			loggerConsultar = createLogger("src/results/" + demand + "/" + listOfDevices[2] + "/" + TEMP_FILE_PREFIX
 					+ listOfDevices[2] + ".csv");
+			this.deleteBin.add("src/results/" + demand + "/" + listOfDevices[2] + "/" + TEMP_FILE_PREFIX
+					+ listOfDevices[2] + ".csv");
+
 			loggerAlterar = createLogger("src/results/" + demand + "/" + listOfDevices[3] + "/" + TEMP_FILE_PREFIX
 					+ listOfDevices[3] + ".csv");
+			this.deleteBin.add("src/results/" + demand + "/" + listOfDevices[3] + "/" + TEMP_FILE_PREFIX
+					+ listOfDevices[3] + ".csv");
+
 			loggerBD = createLogger("src/results/" + demand + "/" + listOfDevices[4] + "/" + TEMP_FILE_PREFIX
 					+ listOfDevices[4] + ".csv");
+			this.deleteBin.add("src/results/" + demand + "/" + listOfDevices[4] + "/" + TEMP_FILE_PREFIX
+					+ listOfDevices[4] + ".csv");
+
 
 			String line;
 			while ((line = this.reader.readLine()) != null) {

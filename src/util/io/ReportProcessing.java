0b894600011c9eac6b72a7d3d5317efd745ab688
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-08-27 03:27:26 -0300

    Further results generated

--- src/util/io/ReportProcessing.java
+++ src/util/io/ReportProcessing.java
@@ -5,10 +5,10 @@ public class ReportProcessing {
 	public static void main(String[] args) {
 		ReportProcessor rp = new ReportProcessor();
 		MetricGenerator mg = new MetricGenerator();
-		String reportPath = "src/reports/highDemandResults.csv";
-		String batchName = "highDemand";
-		Double origRate = 1.3988;
-		Double feedBackRate = 0.20;
+		String reportPath = "src/reports/batch8.csv";
+		String batchName = "batch8";
+		Double origRate = 1.538;
+		Double feedBackRate = 0.14;
 		
 		rp.processReport(reportPath, batchName);
 		mg.processAllDevices(batchName, ReportProcessor.DEVICE_NAMES.split(","), origRate, feedBackRate);
